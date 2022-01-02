1aacccdd0bfa1b6bd0e17b5ad6721b3c498d6e0d
megann <megann.elizabeth.king@gmail.com>
2022-01-01 19:10:45 -0500

    commit tree ec9b891e7cca7024dc3e529f342075ddd872c67d

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Megann"
+    end
+
+    get '/hometown' do
+        "My hometown is Everett, PA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Gravity Waves' by Infected Mushroom"
+    end
+
 end
